490927da2bee0a8c719bf308df0f74060dea477e
nicklefritz <nrcoover@gmail.com>
2025-11-03 10:23:04 -0500

    Create and bind event handlers for form input

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,10 @@ class ProjectInput {
 	hostElement: HTMLDivElement;
 	element: HTMLFormElement;
 
+	titleInputElement: HTMLInputElement;
+	descriptionInputElement: HTMLInputElement;
+	peopleInputElement: HTMLInputElement;
+
 	constructor() {
 		this.templateElement = <HTMLTemplateElement>(
 			document.getElementById("project-input")!
@@ -14,11 +18,35 @@ class ProjectInput {
 			true
 		);
 		this.element = <HTMLFormElement>importedNode.firstElementChild;
+		this.element.id = "user-input";
+
+		this.titleInputElement = <HTMLInputElement>(
+			this.element.querySelector("#title")
+		);
+		this.descriptionInputElement = <HTMLInputElement>(
+			this.element.querySelector("#description")
+		);
+		this.peopleInputElement = <HTMLInputElement>(
+			this.element.querySelector("#people")
+		);
+
+		this.configure();
 		this.attach();
 	}
 
 	private attach() {
 		this.hostElement.insertAdjacentElement("afterbegin", this.element);
+	}
+
+	private submitHandler(event: Event) {
+		event.preventDefault();
+		console.log(this.titleInputElement.value);
+		console.log(this.descriptionInputElement.value);
+		console.log(this.peopleInputElement.value);
+	}
+
+	private configure() {
+		this.element.addEventListener("submit", this.submitHandler.bind(this));
 	}
 }
 
